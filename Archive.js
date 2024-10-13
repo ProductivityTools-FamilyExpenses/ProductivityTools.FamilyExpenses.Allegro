0d97b9e853d83ede075ec58eab23885f81f43851
Pawel Wujczyk <pwujczyk@hotmail.com>
2024-10-13 21:32:15 +0200

    archive

--- Archive.js
+++ Archive.js
@@ -1,5 +1,8 @@
-function myFunction() {
+function runner1() {
   var trixUrl = "https://docs.google.com/spreadsheets/d/1sBC7PWM7DkCA4smf11Gg59tWT5R7JRIRaWqqpkIYgw8/edit?gid=0#gid=0"
+  archiveElementsRowsOlderThan4Monhts(trixUrl)
+}
+function archiveElementsRowsOlderThan4Monhts(trixUrl) {
   var sheetpurchases = getSheet(trixUrl, "WorkingCopyPuchases")
   var sheetArchive = getSheet(trixUrl, "Archive")
   var data = sheetpurchases.getDataRange().getValues();
@@ -10,11 +13,17 @@ function myFunction() {
     if (purchaseDate < referencedate) {
       var rng = sheetpurchases.getRange(i, 1, 1, 8)
       var rangeArray = rng.getValues();
-
-      sheetArchive.appendRow(rangeArray[0]);
+      sheetArchive.appendRow([rangeArray[0][0].toISOString().split('T')[0]].concat(rangeArray[0].slice(1, 9)));
       sheetpurchases.deleteRow(i);
       console.log(rangeArray)
-
     }
   }
+  sort(trixUrl)
+}
+
+
+function sort(trixUrl) {
+  var sheetpurchases = getSheet(trixUrl, "WorkingCopyPuchases")
+  var data = sheetpurchases.getRange('Archive!A:H')
+  data.sort(1);
 }
